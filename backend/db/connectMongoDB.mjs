dbfea33ddcdc202adece1460ac15ff0e4471afd7
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-12-18 20:37:18 +0100

    Update connectMongoDB.mjs

--- backend/db/connectMongoDB.mjs
+++ backend/db/connectMongoDB.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectMongoDB = async () => {
     try{
-        const conn = await  mongoose.connect("mongodb://localhost:27017")
+        const conn = await  mongoose.connect("mongodb+srv://michaelisraelmike:vqczS6Oi8xZth1vQ@cluster0.dzzbs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         
         /* const conn = await mongoose.connect("mongodb://localhost:27017") */
         console.log("connected to mongodb", conn.connection.host)
