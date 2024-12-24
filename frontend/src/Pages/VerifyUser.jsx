5a70f7bd42c1e238580e11f7d12031a62c047315
Michael Israel <157503371+michee2x@users.noreply.github.com>
2024-12-24 10:10:52 +0100

    Update VerifyUser.jsx

--- frontend/src/Pages/VerifyUser.jsx
+++ frontend/src/Pages/VerifyUser.jsx
@@ -24,7 +24,7 @@ const VerifyUser = () => {
     const verify = async () => {
         setLoading(true)
         try{
-            const res = await fetch(`https://wixy-app.onrender.com/api/auth/verify?token=${token}`,{
+            const res = await fetch(`https://wixy-backend.onrender.com/api/auth/verify?token=${token}`,{
                 method:"GET",
                 credentials:"include"
             })
